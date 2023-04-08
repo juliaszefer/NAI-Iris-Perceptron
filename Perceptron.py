b8136154910f1ff312d96cb96fb810452e27c3b6
Julia <julia.szefer@gmail.com>
2023-04-08 15:46:23 +0200

    methods description and bug fixes

--- Perceptron.py
+++ Perceptron.py
@@ -1,20 +1,23 @@
 class Perceptron:
-    def __init__(self, length, a, prog, v_type, start):
+    def __init__(self, length, a, prog, start):
         self.vectorwag = list()
         for i in range(length):
             self.vectorwag.append(start)
         self.a = a
         self.prog = prog
-        self.v_type = v_type
 
+# metoda sprawdzajaca czy perceptron zostal juz nauczony na bazie iloczynu skalarnego
+# podanego wektora i wektora wag i tego czy jest wiekszy lub rowny progowi perceptronu
     def czynauczony(self, vector):
         if len(vector) != len(self.vectorwag):
-            quit("Wektory i wektor wag nie sa tej samej dlugosci")
+            quit("Podany wektor i wektor wag nie sa tej samej dlugosci")
         suma = 0
         for i in range(len(vector)):
             suma += vector[i] * self.vectorwag[i]
+        # print(f"{suma} >= {self.prog}")
         return suma >= self.prog
 
+# nauczanie na bazie ponizszego wzoru
 # W' = W + (d-y) * alfa * X
     def naucz(self, przewidywana, otrzymana, vector):
         rightside = (przewidywana - otrzymana)*self.a
@@ -22,4 +25,5 @@ class Perceptron:
             vector[i] = vector[i]*rightside
         for i in range(len(vector)):
             self.vectorwag[i] += vector[i]
+        # print(self.vectorwag)
         self.prog += rightside*(-1)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+"""
+Iris - perceptron training
+"""
+
 from Iris import Iris
 from Perceptron import Perceptron
+
+
+# Wprowadzanie danych przy odpaleniu programu z terminala
 
 # import sys
 #
@@ -7,21 +14,26 @@ from Perceptron import Perceptron
 #     print('nieprawidlowa ilosc argumentow')
 #     raise TypeError(f"nieprawidlowa ilosc argumentow\nwymagane: 4\notrzymane: {len(sys.argv)}")
 #
-# a = int(sys.argv[1])
+# a = float(sys.argv[1])
 # trainSet = sys.argv[2]
 # testSet = sys.argv[3]
 
 
+# Dane testowe
+
 a = 0.5
 trainSet = "Data/trainSet.txt"
 testSet = "Data/testSet.txt"
+
 accuracySet = "Data/accuracySet.txt"
 
 
+# Metoda sprawdzajaca poprawnosc programu
 def howaccurate(correctno, allno):
     return correctno/allno*100
 
 
+# Metoda sortująca linie z pliku csv oraz tworzaca obiekt klasy Iris
 def sortline(v_line):
     v_type = "default"
     info = v_line.split(",")
@@ -35,6 +47,7 @@ def sortline(v_line):
     return iris1
 
 
+# Metoda odczytujaca dane z podanego pliku i zapisujaca linie do listy, zwraca liste linii z pliku
 def readfile(path):
     arr = list()
     ffile = open(path, 'r')
@@ -43,6 +56,8 @@ def readfile(path):
     return arr
 
 
+# Metoda, ktorej zadaniem jest podjecie decyzji co do typu Irysa,
+# decyzja jest podejmowana na bazie zmiennej typu bool przekazanej jako parametr metody
 def decision(isgood):
     if isgood:
         return "setosa"
@@ -50,14 +65,21 @@ def decision(isgood):
         return "versicolor"
 
 
+# Metoda, której zadaniem jest trenowanie perceptronu na bazie listy treningowej
+# trening polega na sprawdzeniu czy metoda czynauczony zwraca wartosc 0 czy 1 i na tej podstawie przekazaniu
+# tej wartosci do metody decyzyjnej
 def train(perceptronn, v_trainlist):
     czydalejj = True
     while czydalejj:
         good, setosagood, versicolorgood = 0, 0, 0
         for trainiris in v_trainlist:
             v_type = trainiris.v_type
+            parts = v_type.split("-")
+            v_type = parts[1]
             czynauczon = perceptronn.czynauczony(trainiris.vector)
+            # print(czynauczon)
             odp = decision(czynauczon)
+            # print(f"{odp} == {v_type}")
             if odp == v_type:
                 good += 1
                 if odp == "setosa":
@@ -67,9 +89,9 @@ def train(perceptronn, v_trainlist):
             else:
                 perceptronn.naucz(not czynauczon, czynauczon, trainiris.vector)
         accuracy = howaccurate(good, len(v_trainlist))
-        print(f"Ogolna poprawnosc: {accuracy}\n"
-              f"Poprawnosc setosa: {howaccurate(setosagood, len(v_trainlist)/2)}\n"
-              f"Poprawnosc versicolor: {howaccurate(versicolorgood, len(v_trainlist)/2)}")
+        print(f"Ogolna poprawnosc: {accuracy}%\n"
+              f"Poprawnosc setosa: {howaccurate(setosagood, len(v_trainlist)/2)}%\n"
+              f"Poprawnosc versicolor: {howaccurate(versicolorgood, len(v_trainlist)/2)}%")
         if accuracy > 99:
             czydalejj = False
         else:
@@ -78,29 +100,39 @@ def train(perceptronn, v_trainlist):
                 czydalejj = False
 
 
+# Metoda, której zadaniem jest sprawdzenie na testowej liscie czy perceptron nauczyl sie
+# rozpoznawac Irysy, drukujaca jego dokladnosc po przejsciu calej listy testowej
 def test(perceptronn, v_testlist, v_accuracylist):
     good, setosagood, versicolorgood = 0, 0, 0
     for i in range(len(v_testlist)):
         czynauczon = perceptronn.czynauczony(v_testlist[i].vector)
         odp = decision(czynauczon)
-        if odp == v_accuracylist[i].v_type:
+        n_type = v_accuracylist[i].v_type
+        parts = n_type.split("-")
+        n_type = parts[1]
+        if odp == n_type:
             good += 1
             if odp == "setosa":
                 setosagood += 1
             elif odp == "versicolor":
                 versicolorgood += 1
     accuracy = howaccurate(good, len(v_testlist))
-    print(f"Ogolna poprawnosc: {accuracy}\n"
-          f"Poprawnosc setosa: {howaccurate(setosagood, len(v_testlist) / 2)}\n"
-          f"Poprawnosc versicolor: {howaccurate(versicolorgood, len(v_testlist) / 2)}")
+    print(f"Ogolna poprawnosc: {accuracy}%\n"
+          f"Poprawnosc setosa: {howaccurate(setosagood, len(v_testlist) / 2)}%\n"
+          f"Poprawnosc versicolor: {howaccurate(versicolorgood, len(v_testlist) / 2)}%")
 
 
 trainlist = readfile(trainSet)
 testlist = readfile(testSet)
 accuracylist = readfile(accuracySet)
 
-perceptron = Perceptron(len(trainlist[0].vector), a, 1, "default", 1)
+
+print("\nWitamy w programie do rozpoznawania podgatunkow Irysow")
+perceptron = Perceptron(len(trainlist[0].vector), a, 1, 2)
+print(f"\n#######################\n\ttryb nauczania\n#######################\n")
 train(perceptron, trainlist)
+print("\nNauczanie zostało zakończone.")
+print(f"\n#######################\n\ttryb testowania\n#######################\n")
 test(perceptron, testlist, accuracylist)
 
 czydalej = True
@@ -116,5 +148,5 @@ while czydalej:
         typ = decision(czynau)
         print(f"Podany podgatunek irysa to: {typ}")
     else:
-        print("Dziekuje za skorzystanie z programu")
+        print("\nDziekujemy za skorzystanie z programu")
         czydalej = False
